4eb9f24ceddeb6b430f94ecf07eb0c9141c7567f
Chang K Hong <changki.hong@gmail.com>
2021-02-23 05:38:07 +0000

    Udacity Project1 route_planner - implemented AddNeighbors

--- CppND-Route-Planning-Project/src/route_planner.cpp
+++ CppND-Route-Planning-Project/src/route_planner.cpp
@@ -27,13 +27,22 @@ float RoutePlanner::CalculateHValue(RouteModel::Node const *node) {
 
 // TODO 4: Complete the AddNeighbors method to expand the current node by adding all unvisited neighbors to the open list.
 // Tips:
-// - Use the FindNeighbors() method of the current_node to populate current_node.neighbors vector with all the neighbors.
-// - For each node in current_node.neighbors, set the parent, the h_value, the g_value. 
-// - Use CalculateHValue below to implement the h-Value calculation.
-// - For each node in current_node.neighbors, add the neighbor to open_list and set the node's visited attribute to true.
-
 void RoutePlanner::AddNeighbors(RouteModel::Node *current_node) {
+  // - Use the FindNeighbors() method of the current_node to populate current_node.neighbors vector with all the neighbors.
+  current_node->FindNeighbors();
 
+  // - For each node in current_node.neighbors, set the parent, the h_value, the g_value.
+  for(auto neighbor : current_node->neighbors){
+    neighbor->parent = current_node;
+    neighbor->g_value = current_node->g_value + current_node->distance(*neighbor);
+
+    // - Use CalculateHValue below to implement the h-Value calculation.
+    neighbor->h_value = this->CalculateHValue(neighbor);
+    
+    // - For each node in current_node.neighbors, add the neighbor to open_list and set the node's visited attribute to true.
+    open_list.push_back(neighbor);
+    neighbor->visited = true;
+  }
 }
 
 
