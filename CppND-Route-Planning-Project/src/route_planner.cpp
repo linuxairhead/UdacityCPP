d32376cde5b140bd3767acb4e80aaeedb4c791c9
Chang K Hong <changki.hong@gmail.com>
2021-02-25 06:49:16 +0000

    Udacity Project1 route_planner - implemented ConstructFinalPath

--- CppND-Route-Planning-Project/src/route_planner.cpp
+++ CppND-Route-Planning-Project/src/route_planner.cpp
@@ -51,7 +51,6 @@ bool Compare(const RouteModel::Node *aNode, const RouteModel::Node *bNode) {
 
 // TODO 5: Complete the NextNode method to sort the open list and return the next node.
 // Tips:
-
 RouteModel::Node *RoutePlanner::NextNode() {
   // - Sort the open_list according to the sum of the h value and g value.
   std::sort(open_list.begin(), open_list.end(), Compare);
@@ -66,22 +65,25 @@ RouteModel::Node *RoutePlanner::NextNode() {
 
 // TODO 6: Complete the ConstructFinalPath method to return the final path found from your A* search.
 // Tips:
-// - This method should take the current (final) node as an argument and iteratively follow the 
-//   chain of parents of nodes until the starting node is found.
-// - For each node in the chain, add the distance from the node to its parent to the distance variable.
-// - The returned vector should be in the correct order: the start node should be the first element
-//   of the vector, the end node should be the last element.
-
 std::vector<RouteModel::Node> RoutePlanner::ConstructFinalPath(RouteModel::Node *current_node) {
     // Create path_found vector
     distance = 0.0f;
     std::vector<RouteModel::Node> path_found;
 
     // TODO: Implement your solution here.
+    // - This method should take the current (final) node as an argument and iteratively follow the
+    //   chain of parents of nodes until the starting node is found.
+    while(current_node != start_node) {
+      // - For each node in the chain, add the distance from the node to its parent to the distance variable.
+      distance += current_node->distance(*current_node->parent);
+      // - The returned vector should be in the correct order: the start node should be the first element
+      path_found.push_back(*current_node);
+      //   of the vector, the end node should be the last element.
+      current_node = current_node->parent;
+    }
 
     distance *= m_Model.MetricScale(); // Multiply the distance by the scale of the map to get meters.
     return path_found;
-
 }
 
 
