77b1578926dcd00247adbd88de052ae700730943
Chang K Hong <changki.hong@gmail.com>
2021-03-24 03:59:08 +0000

    Udacity Project2 system - enable MemoryUtilization

--- CppND-System-Monitor/src/linux_parser.cpp
+++ CppND-System-Monitor/src/linux_parser.cpp
@@ -68,7 +68,27 @@ vector<int> LinuxParser::Pids() {
 }
 
 // TODO: Read and return the system memory utilization
-float LinuxParser::MemoryUtilization() { return 0.0; }
+float LinuxParser::MemoryUtilization() {
+  string line, key, value;
+  float memtotal=0.0, memfree=0.0;
+
+  std::ifstream filestream(kProcDirectory + kMeminfoFilename);
+  if (filestream.is_open()) {
+    while (std::getline(filestream, line)) {
+      std::istringstream linestream(line);
+
+      while (linestream >> key >> value) {
+        if(key == "MemTotal:"){
+          memtotal = stof(value);
+        }else if(key == "MemFree:"){
+          memfree = stof(value);
+          break;
+        }  
+      }
+    }
+  }
+  return ((memtotal - memfree)/memtotal);
+}
 
 // TODO: Read and return the system uptime
 long LinuxParser::UpTime() { return 0; }
