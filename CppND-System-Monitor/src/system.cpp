b75f3ae1e41c925a0a0d3418150317b1a059fa3e
Chang K Hong <changki.hong@gmail.com>
2021-03-26 04:25:50 +0000

    Udacity Project2 system - get Process's PIDs

--- CppND-System-Monitor/src/system.cpp
+++ CppND-System-Monitor/src/system.cpp
@@ -18,7 +18,16 @@ using std::vector;
 Processor& System::Cpu() { return cpu_; }
 
 // TODO: Return a container composed of the system's processes
-vector<Process>& System::Processes() { return processes_; }
+vector<Process>& System::Processes() {
+  vector<int> pids = LinuxParser::Pids();
+  processes_.clear();
+
+  for (unsigned int i = 0; i < pids.size(); i++) {
+      Process process(pids[i]);
+      processes_.push_back(process);
+  }
+  return processes_;
+}
 
 // TODO: Return the system's kernel identifier (string)
 std::string System::Kernel() { return LinuxParser::Kernel(); }
