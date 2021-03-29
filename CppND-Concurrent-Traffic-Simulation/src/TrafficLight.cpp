05106b8de20fe6bdc45665322c923d38a1486a38
Chang K Hong <changki.hong@gmail.com>
2021-03-29 01:36:44 +0000

    Udacity Project4 TrafficLight - implemented cycleThroughPhases, simulate method

--- CppND-Concurrent-Traffic-Simulation/src/TrafficLight.cpp
+++ CppND-Concurrent-Traffic-Simulation/src/TrafficLight.cpp
@@ -22,6 +22,7 @@ void MessageQueue<T>::send(T &&msg)
 */
 
 /* Implementation of class "TrafficLight" */
+
 TrafficLight::TrafficLight()
 {
     _currentPhase = TrafficLightPhase::red;
@@ -43,7 +44,8 @@ TrafficLightPhase TrafficLight::getCurrentPhase()
 
 void TrafficLight::simulate()
 {
-    // FP.2b : Finally, the private method „cycleThroughPhases“ should be started in a thread when the public method „simulate“ is called. To do this, use the thread queue in the base class. 
+    // FP.2b : Finally, the private method „cycleThroughPhases“ should be started in a thread when the public method „simulate“ is called. To do this, use the thread queue in the base class.
+    threads.emplace_back(std::thread(&TrafficLight::cycleThroughPhases, this));
 }
 
 // virtual function which is executed in a thread
@@ -53,4 +55,23 @@ void TrafficLight::cycleThroughPhases()
     // and toggles the current phase of the traffic light between red and green and sends an update method 
     // to the message queue using move semantics. The cycle duration should be a random value between 4 and 6 seconds. 
     // Also, the while-loop should use std::this_thread::sleep_for to wait 1ms between two cycles. 
+
+    // random generator
+    std::random_device device;
+    std::mt19937 generator(device());
+    //The cycle duration should be a random value between 4 and 6 seconds.
+    std::uniform_real_distribution<double> dist(4.0, 6.0);
+
+    while (true) {
+      float cycleDuration = dist(generator);
+
+      //the time between two loop cycles
+      auto start = std::chrono::high_resolution_clock::now();
+      std::this_thread::sleep_for(std::chrono::milliseconds(1)); //to wait 1ms between two cycles.
+      auto deltaTime = std::chrono::high_resolution_clock::now() - start;
+
+      if(deltaTime.count() > cycleDuration) {
+        _currentPhase = (_currentPhase == TrafficLightPhase::green) ? red : green;
+      }
+    }
 }
