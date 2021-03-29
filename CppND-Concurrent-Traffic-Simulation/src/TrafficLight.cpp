4dcef2343533d195926af77a10f1f4d7ee057a30
Chang K Hong <changki.hong@gmail.com>
2021-03-29 04:21:06 +0000

    Udacity Project4 TrafficLight - implement the receive method and waitForGreen method

--- CppND-Concurrent-Traffic-Simulation/src/TrafficLight.cpp
+++ CppND-Concurrent-Traffic-Simulation/src/TrafficLight.cpp
@@ -8,8 +8,16 @@ template <typename T>
 T MessageQueue<T>::receive()
 {
     // FP.5a : The method receive should use std::unique_lock<std::mutex> and _condition.wait()
+    std::unique_lock<std::mutex> ulock(_mutex);
+    _condition.wait(ulock, [this]{
+       return !_queue.empty();
+    });
     // to wait for and receive new messages and pull them from the queue using move semantics. 
+    T msg = std::move(_queue.back());
     // The received object should then be returned by the receive function.
+    _queue.pop_front();
+
+    return msg;
 }
 
 template <typename T>
@@ -37,6 +45,13 @@ void TrafficLight::waitForGreen()
     // FP.5b : add the implementation of the method waitForGreen, in which an infinite while-loop 
     // runs and repeatedly calls the receive function on the message queue. 
     // Once it receives TrafficLightPhase::green, the method returns.
+    while (true)
+    {
+        std::this_thread::sleep_for(std::chrono::milliseconds(1));
+        auto msg = _queue.receive();
+        if(msg == green)
+            return;
+    }
 }
 
 TrafficLightPhase TrafficLight::getCurrentPhase()
